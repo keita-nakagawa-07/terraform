1fe1624b9d6dddae628886b53795b52fa0a390f2
keita nakagawa <keita1967@gmail.com>
2026-09-07 17:39:49 +0900

    fix main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_storage_bucket" "test" {
+  name          = "${var.project_id}-test-bucket"
+  location      = var.region
+  force_destroy = true
+}
