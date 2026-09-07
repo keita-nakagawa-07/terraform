ad6209abe86f17bf409f912be55fc0b128217d0f
keita nakagawa <keita1967@gmail.com>
2026-09-07 20:49:25 +0900

    sts setup phase1

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -1,9 +1,30 @@
-variable "project_id" {
-  type    = string
-  default = "default-155223"
+variable "azure_storage_account" {
+  type        = string
+  description = "keitaststest"
 }
 
-variable "region" {
-  type    = string
-  default = "asia-northeast1"
+variable "azure_container" {
+  type        = string
+  description = "sts"
+}
+
+variable "azure_path" {
+  type        = string
+  default     = ""
+  description = "/public"
+}
+
+variable "azure_client_id" {
+  type        = string
+  description = "Azure ADアプリ(フェデレーテッドクレデンシャル用)のクライアントID"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "1c565538-5a72-4dc2-81e3-610bdc5dab9a"
+}
+
+variable "gcs_sink_bucket" {
+  type        = string
+  description = "default-155223-test-bucket"
 }
